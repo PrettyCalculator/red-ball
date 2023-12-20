4a276a0cb999bfa3606a4246791524ea8f8d5d5f
PrettyCalculator <ssooddaa2031@gmail.com>
2023-12-21 00:30:33 +0500

    анимация шарика (через 45)

--- player.py
+++ player.py
@@ -5,8 +5,16 @@ from functions import load_image
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = load_image('img2.png')
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.images = []
+        self.delay = 0
+        c = 0
+        self.cur_frame = 0
+        for i in range(8):
+            image = load_image(f'img2_{c}.png')
+            image = pygame.transform.scale(image, (50, 50))
+            self.images.append(image)
+            c += 45
+        self.image = self.images[self.cur_frame]
         self.rect = self.image.get_rect(topleft=pos)
 
         self.direction = pygame.math.Vector2(0, 0)
@@ -20,8 +28,10 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.anim(1)
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.anim(-1)
         else:
             self.direction.x = 0
         if keys[pygame.K_UP] and not self.is_jump:
@@ -34,6 +44,19 @@ class Player(pygame.sprite.Sprite):
 
     def jump(self):
         self.direction.y = self.jump_speed
+
+    def anim(self, direction):
+        if self.delay == 2:
+            if self.cur_frame == 7 and direction > 0:
+                self.cur_frame = 0
+            elif self.cur_frame == 0 and direction < 0:
+                self.cur_frame = 7
+            else:
+                self.cur_frame += direction
+            self.image = self.images[self.cur_frame]
+            self.delay = 0
+        else:
+            self.delay += 1
 
     def update(self):
         self.get_input()
