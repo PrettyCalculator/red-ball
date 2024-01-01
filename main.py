7bf02f25b3fc7fca6e03a0204b0dad7e64bd11af
User <karterr.r@yandex.ru>
2024-01-01 19:13:34 +0500

    play, exit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from level import Level
 from homescreen import HomeScreen
+from functions import load_image
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -12,11 +13,12 @@ pygame.display.set_caption("Шарик")
 home = HomeScreen(homescreen_map, screen)
 game = Level(level_map, screen)
 clock = pygame.time.Clock()
-
+bg_surf = load_image('background.jpg')
 running = True
 fps = 60
 while running:
     from settings import mode
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -24,7 +26,8 @@ while running:
         screen.fill(pygame.Color("#87cefa"))
         home.run()
     elif mode == 'game':
-        screen.fill(pygame.Color('gray'))
+        bg_surf = pygame.transform.scale(bg_surf, (screen_width, screen_height))
+        screen.blit(bg_surf, (0, 0))
         game.run()
     elif mode == 'exit':
         running = False
